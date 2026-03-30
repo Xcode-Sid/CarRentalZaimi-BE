e67942dedf29d6b0834b7de5ca59fff0cec25a3a
HP <HP@DESKTOP-HB4GN2T>
2026-03-30 13:54:31 +0200

    fixing ApplicationDbContext

--- AutoZaimi.Infrastructure/Persistence/ApplicationDbContext.cs
+++ AutoZaimi.Infrastructure/Persistence/ApplicationDbContext.cs
@@ -11,22 +11,42 @@ public class ApplicationDbContext : IdentityDbContext<User, Role, string>
 
 
     //Entities
-    public DbSet<Car> Cars => Set<Car>();
-    public DbSet<CarCategory> CarCategories => Set<CarCategory>();
-    public DbSet<CarImages> CarImages => Set<CarImages>();
-    public DbSet<CarReview> CarReviews => Set<CarReview>();
-    public DbSet<CarFuel> CarFuels => Set<CarFuel>();
-    public DbSet<CarCompanyModel> CarCompanyModels => Set<CarCompanyModel>();
-    public DbSet<CarCompanyName> CarCompanyNames => Set<CarCompanyName>();
-    public DbSet<CarExteriorColor> CarExteriorColors => Set<CarExteriorColor>();
-    public DbSet<CarInteriorColor> CarInteriorColors => Set<CarInteriorColor>();
-    public DbSet<CarTransmission> CarTransmissions => Set<CarTransmission>();
-    public DbSet<Booking> Bookings => Set<Booking>();
-    public DbSet<GoogleReview> GoogleReviews => Set<GoogleReview>();
+    public DbSet<AdditionalService> AdditionalServices { get; set; }
+    public DbSet<Booking> Bookings { get; set; }
+    public DbSet<BookingService> BookingServices { get; set; }
+    public DbSet<Car> Cars { get; set; }
+    public DbSet<CarCategory> CarCategories { get; set; }
+    public DbSet<CarCompanyModel> CarCompanyModels { get; set; }
+    public DbSet<CarCompanyName> CarCompanyNames { get; set; }
+    public DbSet<CarExteriorColor> CarExteriorColors { get; set; }
+    public DbSet<CarFuel> CarFuels { get; set; }
+    public DbSet<CarImages> CarImages { get; set; }
+    public DbSet<CarInteriorColor> CarInteriorColors { get; set; }
+    public DbSet<CarReview> CarReviews { get; set; }
+    public DbSet<CarTransmission> CarTransmissions { get; set; }
+    public DbSet<CompanyProfile> CompanyProfiles { get; set; }
+    public DbSet<ContactMessage> ContactMessages { get; set; }
+    public DbSet<EmailConfirmationToken> EmailConfirmationTokens { get; set; }
+    public DbSet<GoogleReview> GoogleReviews { get; set; }
+    public DbSet<Language> Languages { get; set; }
+    public DbSet<PhoneConfirmationToken> PhoneConfirmationTokens { get; set; }
+    public DbSet<Promotion> Promotions { get; set; }
+    public DbSet<RefreshToken> RefreshTokens { get; set; }
+    public DbSet<SavedCar> SavedCars { get; set; }
+    public DbSet<UserImage> UserImages { get; set; }
+    public DbSet<UserNotification> UserNotifications { get; set; }
 
 
     protected override void OnModelCreating(ModelBuilder builder)
     {
         base.OnModelCreating(builder);
+
+        builder.Entity<CompanyProfile>()
+         .Property(e => e.WhyChooseUs)
+         .HasColumnType("json");
+
+        builder.Entity<CompanyProfile>()
+         .Property(e => e.WorkingHours)
+         .HasColumnType("json");
     }
 }
